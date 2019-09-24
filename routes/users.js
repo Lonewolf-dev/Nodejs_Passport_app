2ec58e26146d7768cc1c9e873e7407099b460549
Lonewolf-dev <43385218+Nathanos21@users.noreply.github.com>
2019-09-24 16:42:28 +0200

    first update

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+//Login page
+router.get('/login', (req, res) => res.render('login'));
+
+//Register page
+router.get('/register', (req, res) => res.render('register'));
+
+module.exports= router;
